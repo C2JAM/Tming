3e91c3c8f0a2f1b264e897fadf3731e978ab775a
luceinaltis <luceinaltis2020@naver.com>
2020-11-29 22:05:43 +0900

    update

--- src/pages/Vote/votedUsers.js
+++ src/pages/Vote/votedUsers.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
@@ -7,22 +7,63 @@ const Wrapper = styled.div`
   width: 100%;
   border-right: #222831 3px solid;
   display: grid;
+  padding: 20px;
+  grid-gap: 10px;
   grid-template:
-    'top' 100px
+    'top' 60px
     '.' 1fr
     / 1fr;
 
   .top {
-    padding: 20px;
+    background-color: #ffd369;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #222831;
   }
 `;
 
+const VotedUser = styled.div`
+  background-color: #eeeeee;
+  color: #222831;
+  font-size: 18px;
+  height: 60px;
+  padding: 10px;
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+`;
+
 function VotedUsers() {
+  const [votedUsers, setvotedUsers] = useState(
+    JSON.parse(window.localStorage.getItem('votedUsers')),
+  );
+
+  useEffect(() => {
+    function onChangeVotedUsers() {
+      const newVotedUsers = JSON.parse(
+        window.localStorage.getItem('votedUsers'),
+      );
+
+      setvotedUsers(newVotedUsers);
+    }
+
+    const IntervalId = setInterval(onChangeVotedUsers, 1000);
+
+    return () => {
+      clearInterval(IntervalId);
+    };
+  }, []);
+
   return (
     <>
       <Wrapper>
-        <div className="top">투표 참여자 : </div>
-        <div>밑</div>
+        <div className="top">투표 참여자 : {votedUsers.length}</div>
+        <div>
+          {votedUsers.map(value => {
+            return <VotedUser>{value.userName}</VotedUser>;
+          })}
+        </div>
       </Wrapper>
     </>
   );
